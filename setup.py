a513e767fa20e5b40020e050df8fee67080b30e4
Hongli Lai <h.lai@fullstaq.com>
2021-01-12 12:58:39 +0100

    setup.py: specify correct Python version requirement

--- setup.py
+++ setup.py
@@ -33,5 +33,5 @@ setuptools.setup(
         'License :: OSI Approved :: Apache License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
