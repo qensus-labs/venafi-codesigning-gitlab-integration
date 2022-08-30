5e3e1708369f94439bec95a97fd1537d43a61847
Greg Brownstein <greg@jagtechnical.com>
2022-08-29 21:01:53 -0400

    fix msi file name

--- images/signtool/install-venafi-client-tools.ps1
+++ images/signtool/install-venafi-client-tools.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Set-PSDebug -Trace 1
 
 $proc = Start-Process -Wait -NoNewWindow -PassThru msiexec `
-    -ArgumentList ('/i','\build\VenafiCodeSigningClients-x64.msi', `
+    -ArgumentList ('/i','\build\VenafiCodeSigningClients.msi', `
     '/passive','/l*vx','\build\venafi-install.log')
 if ($proc.ExitCode -ne 0) {
     Get-Content \build\venafi-install.log
