30a39378d119a8d8c4b7bb688e32fb8b74708aff
avwsolutions <Arnold.van.Wijnbergen@Gmail.com>
2022-12-01 21:13:52 +0100

    Updated Python to 3.9 release

--- images/signtool/install-python.ps1
+++ images/signtool/install-python.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Set-PSDebug -Trace 1
 
 Invoke-WebRequest -UseBasicParsing -OutFile \build\python-installer.exe `
-    https://www.python.org/ftp/python/3.8.6/python-3.8.6-amd64.exe
+    https://www.python.org/ftp/python/3.9.13/python-3.9.13-amd64.exe
 
 $proc = Start-Process -Wait -NoNewWindow -PassThru \build\python-installer.exe `
     -ArgumentList ('/quiet','/log','\build\python-installer.log','TargetDir=C:\Python','InstallAllUsers=1','PrependPath=1','Include_doc=0','Include_tcltk=0')
